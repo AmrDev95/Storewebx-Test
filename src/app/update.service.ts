2a6a287d83da1f3b637624eb15898c5a49131c6a
AmrDev95 <103969231+AmrDev95@users.noreply.github.com>
2022-07-13 13:39:37 +0200

    Update update.service.ts

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -15,7 +15,12 @@ export class UpdateService {
     
     fd.append('name',name);
     fd.append('description' ,description );
-    fd.append('image' ,imageUrl, imageUrl.name);
+    if(imageUrl == null){
+      console.log(imageUrl);
+    }
+    else{
+      fd.append('image' ,imageUrl, imageUrl.name);
+    }
     fd.append('category_id' , category_id);
     fd.append('_method', 'put');
     return this._HttpClient.post(`https://test-api.storexweb.com/api/movies/${id}`, fd , {headers:new HttpHeaders() , observe: 'response'});
